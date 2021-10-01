b89b109e6cab9dfa0f32e17970d7188867aeea2a
unknown <aishiteru1509@gmail.com>
2021-10-01 19:09:18 +0700

    new

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,8 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -23,9 +21,10 @@ public class Main extends Application{
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/views/home.fxml")); //container
 			Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();//get screen width, height
-			Scene scene = new Scene(root);
-			primaryStage.setWidth(screenBounds.getWidth());
-			primaryStage.setHeight(screenBounds.getHeight());
+			Scene scene = new Scene(root, 1000.1, 600);
+			scene.getStylesheets().add("/css/customer-home.css");
+			primaryStage.setX((screenBounds.getWidth() - 1000)/2);
+			primaryStage.setY((screenBounds.getHeight() - 600)/2);
 			primaryStage.setResizable(true);
 			primaryStage.setScene(scene);
 			//primaryStage.initStyle(StageStyle.UNDECORATED);
--- src/controllers/CustomerHomeController.java
+++ src/controllers/CustomerHomeController.java
@@ -1,5 +1,24 @@
 package controllers;
 
-public class CustomerHomeController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class CustomerHomeController implements Initializable{
+
+	
+	//btn
+	@FXML
+	private Button btnDessert;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
